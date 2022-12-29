03bb5cc2765ec335cdf1a21646b10f8027669e6d
alex <bigbadmath@gmail.com>
2022-12-28 23:11:29 -0500

    coord mode

--- examples/hot_reload_tide/src/main.rs
+++ examples/hot_reload_tide/src/main.rs
@@ -1,20 +1,33 @@
 use hot_reload_tide::messages::{load_config, Config};
 use notify::{Error, Event, PollWatcher, RecommendedWatcher, RecursiveMode, Watcher};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::collections::HashMap;
 use std::fs::read_dir;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
 use tide::{log, Body, Response};
 
 const CONFIG_PATH: &str = "config.json";
 const SEGMENTS_PATH: &str = "/tmp/segments/";
+const SEGMENTS_VERSION_PATH: &str = "/tmp/segments/version";
+
+type SegmentName = String;
+type Term = String;
+type PostingList = Vec<usize>;
+type InvertedIndex = HashMap<Term, PostingList>;
 
 /// A pretend ii
-type Segment = HashMap<String, Vec<usize>>;
+#[derive(Deserialize, Serialize)]
+pub struct Segment {
+    data: InvertedIndex,
+    version: String,
+}
+type Segments = HashMap<SegmentName, Segment>;
 
-// for initial loading of everything at boot (only)
-pub fn load_segments(path: &str) -> Result<HashMap<String, Segment>, Box<dyn std::error::Error>> {
+// for initial loading of everything at boot and coord mode
+pub fn load_segments(path: &str) -> Result<Segments, Box<dyn std::error::Error>> {
     let paths = read_dir(path)?;
     let mut segments = HashMap::new();
     for p in paths {
@@ -27,15 +40,21 @@ pub fn load_segments(path: &str) -> Result<HashMap<String, Segment>, Box<dyn std
                 .to_str()
                 .expect("valid segment file name")
         );
-        let file = std::fs::File::open(full_p.clone())?;
-        let file_size = file.metadata()?.len();
-        if file_size == 0 {
-            // add more validation here
-            log::info!("The segment file {} is empty. Skipping.", full_p);
+        // better to skip on non-seg prefix
+        if full_p.eq(SEGMENTS_VERSION_PATH) {
+            log::info!("SKIP: {:?}", &full_p);
+            continue;
         } else {
-            let reader = std::io::BufReader::new(file);
-            let segment: Segment = serde_json::from_reader(reader)?;
-            segments.insert(full_p, segment);
+            let file = std::fs::File::open(full_p.clone())?;
+            let file_size = file.metadata()?.len();
+            if file_size == 0 {
+                // add more validation here
+                log::info!("The segment file {} is empty. Skipping.", full_p);
+            } else {
+                let reader = std::io::BufReader::new(file);
+                let segment: Segment = serde_json::from_reader(reader)?;
+                segments.insert(full_p, segment);
+            }
         }
     }
     Ok(segments)
@@ -46,6 +65,13 @@ pub fn load_segments(path: &str) -> Result<HashMap<String, Segment>, Box<dyn std
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     log::start();
+
+    // in coord mode, only drive segment reload through version coordinator
+    let coord = match std::env::var("COORDINATOR_MODE") {
+        Ok(_val) => true,
+        Err(_e) => false,
+    };
+    log::info!("coordination mode is {}", coord);
 
     let config = load_config(CONFIG_PATH).unwrap();
     let segments: HashMap<String, Segment> = load_segments(SEGMENTS_PATH).unwrap();
@@ -92,32 +118,65 @@ async fn main() -> tide::Result<()> {
                 }
                 notify::EventKind::Create(_) => {
                     log::info!("CREATE: {:?}", event.paths);
-                    for p in event.paths {
-                        let file = std::fs::File::open(p.clone()).expect("segment");
-                        let file_size = file.metadata().expect("metadata").len();
-                        if file_size == 0 {
-                            // add more validation here
-                            log::warn!("The segment file {:?} is empty. Skipping.", p);
-                        } else {
-                            let reader = std::io::BufReader::new(file);
-                            let segment: Segment =
-                                serde_json::from_reader(reader).expect("segment");
-                            log::info!("LOAD: {:?}", &p);
-                            // we don't lock until AFTER we have already loaded the structure
-                            let mut segments = c_a_segments.write().unwrap();
-                            segments.insert(p.to_str().expect("valid path").to_string(), segment);
+                    if !coord {
+                        for p in event.paths {
+                            if p.eq(&PathBuf::from(SEGMENTS_VERSION_PATH)) {
+                                log::info!("SKIP: {:?}", &p);
+                                continue;
+                            } else {
+                                let file = std::fs::File::open(p.clone()).expect("segment");
+                                let file_size = file.metadata().expect("metadata").len();
+                                if file_size == 0 {
+                                    // add more validation here
+                                    log::warn!("The segment file {:?} is empty. Skipping.", p);
+                                } else {
+                                    let reader = std::io::BufReader::new(file);
+                                    let segment: Segment =
+                                        serde_json::from_reader(reader).expect("segment");
+                                    log::info!("LOAD: {:?}", &p);
+                                    // we don't lock until AFTER we have already loaded the structure
+                                    let mut segments = c_a_segments.write().unwrap();
+                                    segments.insert(
+                                        p.to_str().expect("valid path").to_string(),
+                                        segment,
+                                    );
+                                }
+                            }
                         }
                     }
                 }
                 notify::EventKind::Modify(_) => {
-                    log::warn!("MODIFY: (unused)");
+                    // simple example of in-place modification
+                    // also could be used to coordinate files
+                    if event.paths.contains(&PathBuf::from(SEGMENTS_VERSION_PATH)) {
+                        let file = std::fs::File::open(SEGMENTS_VERSION_PATH).expect("version");
+                        let reader = std::io::BufReader::new(file);
+                        let v: Value = serde_json::from_reader(reader).expect("version");
+                        log::info!("version: {:?}", v["version"]);
+                        if coord {
+                            log::info!("coord mode on, reloading all segments at once");
+                            let new_segments =
+                                load_segments(SEGMENTS_PATH).expect("valid new segments");
+                            // can validate against updated version
+                            *c_a_segments.write().unwrap() = new_segments;
+                        }
+                    } else {
+                        log::warn!("MODIFY: {:?} (unused)", event.paths);
+                    }
                 }
                 notify::EventKind::Remove(_) => {
                     log::info!("REMOVE: {:?}", event.paths);
-                    for p in event.paths {
-                        log::info!("UNLOAD: {:?}", &p);
-                        let mut segments = c_a_segments.write().unwrap();
-                        segments.remove(&p.to_str().expect("valid path").to_string());
+                    if !coord {
+                        for p in event.paths {
+                            if p.eq(&PathBuf::from(SEGMENTS_VERSION_PATH)) {
+                                log::info!("SKIP: {:?}", &p);
+                                continue;
+                            } else {
+                                log::info!("UNLOAD: {:?}", &p);
+                                let mut segments = c_a_segments.write().unwrap();
+                                segments.remove(&p.to_str().expect("valid path").to_string());
+                            }
+                        }
                     }
                 }
                 notify::EventKind::Other => todo!(),
@@ -175,7 +234,7 @@ async fn search(req: Request) -> tide::Result {
     let segments = &req.state().1.read().unwrap();
     let mut results: Vec<usize> = Vec::new();
     for (_segment_path, segment) in segments.iter() {
-        if let Some(doc_ids) = segment.get(&term) {
+        if let Some(doc_ids) = segment.data.get(&term) {
             for doc_id in doc_ids {
                 results.push(*doc_id);
             }
